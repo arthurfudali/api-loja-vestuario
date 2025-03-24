c840c33174722a80d67422828587ae758f11356f
Arthur Ribeiro Dias Fudali <arthurfudali5@gmail.com>
2025-03-24 15:39:45 -0300

    Create routes

---
+++ routes/storeRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const storeRoutes = express.Router();
+import storeController from "../controllers/storeController.js";
+
+storeRoutes.get("/clothes", storeController.getAllItems);
+storeRoutes.psot("/clothes", storeController.createItem);
+storeRoutes.delete("/clothes:id", storeController.deleteItem);
+storeRoutes.put("/clothes:id", storeController.updateItem);
+storeRoutes.get("/clothes:id", storeController.getOneItem);
+
+export default storeRoutes;
