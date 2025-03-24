bd9b397c68a74ff38fa02ece93df0c3702c1ecfc
Arthur Ribeiro Dias Fudali <arthurfudali5@gmail.com>
2025-03-24 15:04:58 -0300

    Create Clothes.js

---
+++ models/Clothes.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const DescriptionSchema = new mongoose.Schema({
+  size: String,
+  color: String,
+  material: String,
+  brand: String,
+  madeIn: String,
+});
+
+const ClothesSchema = new mongoose.Schema({
+  name: String,
+  type: String,
+  price: Number,
+  description: DescriptionSchema,
+});
+
+const Clothes = mongoose.model("Clothes", ClothesSchema);
+export default Clothes;
